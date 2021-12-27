d84bfd0ee50665b6067da24b37f8c44b44c573ed
Rcenador <rocio.cenador@solidlines.io>
2021-12-27 11:15:20 +0100

    added file generation from DHIS2 data

---
+++ dataToFile.js
@@ -0,0 +1,198 @@
+var fs = require('fs');
+
+const PATIENT_CODE_TEA = "dsWUbqvV9GW";
+//PROGRAMS
+const TARV_UID = "e3swbbSnbQ2";
+const MERE_UID = "MVooF5iCp8L";
+const ENFANT_UID = "PiXg9cX1i0y";
+
+//PROGRAM NAMES
+const TARV = "TARV";
+const MERE = "PTME_MERE";
+const ENFANT = "PTME_ENFANT";
+
+//PROGRAM STAGES
+const PROGRAMSTAGE_TARV_PREMIER_DEBUT_ARV = "R7lNOFLLdIs";
+const PROGRAMSTAGE_TARV_TARV = "CTBhldBbg5x";
+const PROGRAMSTAGE_TARV_CONSULTATION = "sPrQc1FGfP1";
+const PROGRAMSTAGE_TARV_DEBUT_TRAITEMENT_TB = "ZF6rqY3Jmt5";
+const PROGRAMSTAGE_TARV_PERDU_DE_VUE = "aDBN3tSC41E";
+const PROGRAMSTAGE_TARV_SORTIE = "WV6yIfebK6w";
+
+const PROGRAMSTAGE_PTME_MERE_ADMISSION_PTME_MERE = "GCDZcx2b3Lr"
+const PROGRAMSTAGE_PTME_MERE_PREMIERDEBUTARVPTME = "z00BOUSdTuM"
+const PROGRAMSTAGE_PTME_MERE_DELIVERY = "lt2U1bYvz0r"
+const PROGRAMSTAGE_PTME_MERE_SORTIE = "LSIPBEYcQ5R"
+
+const PROGRAMSTAGE_PTME_ENFANT_ADMISSION = "ZWtVELL8YQS"
+const PROGRAMSTAGE_PTME_ENFANT_PCR_INITIAL = "D10B1LUle3y"
+const PROGRAMSTAGE_PTME_ENFANT_PCR_SUIVI = "Dyn481NuzbJ"
+const PROGRAMSTAGE_PTME_ENFANT_SORTIE = "ql4K11mDbzw"
+
+//PROGRAM STAGES NAMES
+const PROGRAMSTAGE_TARV_PREMIER_DEBUT_ARV_LABEL = "PREMIER_DEBUT_ARV"
+const PROGRAMSTAGE_TARV_TARV_LABEL = "TARV"
+const PROGRAMSTAGE_TARV_CONSULTATION_LABEL = "CONSULTATION"
+const PROGRAMSTAGE_TARV_DEBUT_TRAITEMENT_TB_LABEL = "DEBUT_TRAITEMENT_TB"
+const PROGRAMSTAGE_TARV_PERDU_DE_VUE_LABEL = "PERDU_DE_VUE"
+const PROGRAMSTAGE_TARV_SORTIE_LABEL = "SORTIE"
+
+const PROGRAMSTAGE_PTME_MERE_ADMISSION_PTME_MERE_LABEL = "ADMISSION"
+const PROGRAMSTAGE_PTME_MERE_PREMIERDEBUTARVPTME_LABEL = "PREMIERDEBUTARVPTME"
+const PROGRAMSTAGE_PTME_MERE_DELIVERY_LABEL = "DELIVERY"
+const PROGRAMSTAGE_PTME_MERE_SORTIE_LABEL = "SORTIE"
+
+const PROGRAMSTAGE_PTME_ENFANT_ADMISSION_LABEL = "ADMISSION"
+const PROGRAMSTAGE_PTME_ENFANT_PCR_INITIAL_LABEL = "PCR_INITIAL"
+const PROGRAMSTAGE_PTME_ENFANT_PCR_SUIVI_LABEL = "PCR_SUIVI"
+const PROGRAMSTAGE_PTME_ENFANT_SORTIE_LABEL = "SORTIE"
+
+
+//DICT
+var stagesDict = {};
+    stagesDict[PROGRAMSTAGE_TARV_PREMIER_DEBUT_ARV] = PROGRAMSTAGE_TARV_PREMIER_DEBUT_ARV_LABEL
+    stagesDict[PROGRAMSTAGE_TARV_TARV] = PROGRAMSTAGE_TARV_TARV_LABEL
+    stagesDict[PROGRAMSTAGE_TARV_CONSULTATION] = PROGRAMSTAGE_TARV_CONSULTATION_LABEL
+    stagesDict[PROGRAMSTAGE_TARV_DEBUT_TRAITEMENT_TB] = PROGRAMSTAGE_TARV_DEBUT_TRAITEMENT_TB_LABEL
+    stagesDict[PROGRAMSTAGE_TARV_PERDU_DE_VUE] = PROGRAMSTAGE_TARV_PERDU_DE_VUE_LABEL
+    stagesDict[PROGRAMSTAGE_TARV_SORTIE] = PROGRAMSTAGE_TARV_SORTIE_LABEL
+
+    stagesDict[PROGRAMSTAGE_PTME_MERE_ADMISSION_PTME_MERE] = PROGRAMSTAGE_PTME_MERE_ADMISSION_PTME_MERE_LABEL
+    stagesDict[PROGRAMSTAGE_PTME_MERE_PREMIERDEBUTARVPTME] = PROGRAMSTAGE_PTME_MERE_PREMIERDEBUTARVPTME_LABEL
+    stagesDict[PROGRAMSTAGE_PTME_MERE_DELIVERY] = PROGRAMSTAGE_PTME_MERE_DELIVERY_LABEL
+    stagesDict[PROGRAMSTAGE_PTME_MERE_SORTIE] = PROGRAMSTAGE_PTME_MERE_SORTIE_LABEL
+
+    stagesDict[PROGRAMSTAGE_PTME_ENFANT_ADMISSION] = PROGRAMSTAGE_PTME_ENFANT_ADMISSION_LABEL
+    stagesDict[PROGRAMSTAGE_PTME_ENFANT_PCR_INITIAL] = PROGRAMSTAGE_PTME_ENFANT_PCR_INITIAL_LABEL
+    stagesDict[PROGRAMSTAGE_PTME_ENFANT_PCR_SUIVI]= PROGRAMSTAGE_PTME_ENFANT_PCR_SUIVI_LABEL
+    stagesDict[PROGRAMSTAGE_PTME_ENFANT_SORTIE] = PROGRAMSTAGE_PTME_ENFANT_SORTIE_LABEL
+
+const ENFANT_FILE = "DHIS2_data/enfant.json";
+const MERE_FILE = "DHIS2_data/mere.json";
+const TARV_FILE = "DHIS2_data/tarv.json";
+const OUT_FILE = "DHIS2_data/patient_code_uid.json";
+
+//DHIS2 retrieved data
+var dhis_data = [];
+var patient_code_uid = {};
+//***** READ FILES ******/
+var enfant_data = JSON.parse(fs.readFileSync(ENFANT_FILE));
+var mere_data = JSON.parse(fs.readFileSync(MERE_FILE));
+var tarv_data = JSON.parse(fs.readFileSync(TARV_FILE));
+
+//Merge TEIs
+dhis_data = [
+    ...enfant_data,
+    ...mere_data,
+    ...tarv_data
+];
+
+//Fill patient_code_uid
+dhis_data.forEach((TEI) => {
+    //CODE PATIENT key
+    var patient = getPatientCode(TEI);
+
+    if (typeof patient_code_uid[patient] === "undefined") {
+        //TEI uid
+        patient_code_uid[patient] = { "uid": TEI.trackedEntityInstance };
+    }
+
+
+    //Enrollments keys
+    TEI.enrollments.forEach((enroll) => {
+        //Enrollment info
+        var enrollmentLabel = getEnrollmentLabel(enroll);
+        patient_code_uid[patient][enrollmentLabel] = {};
+        patient_code_uid[patient][enrollmentLabel][getDHIS2dateFormat(enroll.enrollmentDate)] = enroll.enrollment;
+
+        //Events info
+        //Extract stage keys
+        var enrollmentUID_label = getEnrollmentUIDLabel(enroll);
+        patient_code_uid[patient][enrollmentUID_label] = {};
+        enroll.events.forEach((event) => {
+            //Check stage
+            var stage = event.programStage;
+            if (typeof patient_code_uid[patient][enrollmentUID_label][stagesDict[stage]] === "undefined") {
+                patient_code_uid[patient][enrollmentUID_label][stagesDict[stage]] = {}
+                patient_code_uid[patient][enrollmentUID_label][stagesDict[stage]] = getEvents_format(enroll.events, stage);
+                console.log();
+            }
+        });
+
+        
+
+    })
+});
+
+
+//Main
+saveJSONFile(OUT_FILE, patient_code_uid);
+
+
+
+/******** FUNCTIONS  *******/
+
+function getPatientCode(TEI) {
+
+    var attribute = TEI.attributes.filter(function (entry) {
+        return entry.attribute === PATIENT_CODE_TEA;
+    });
+    return attribute[0].value;
+}
+
+function getEnrollmentLabel(enrollment) {
+    var program = getProgramName(enrollment.program);
+    return program + "_enrollment";
+}
+
+function getProgramName(programUID) {
+    if (programUID === TARV_UID) {
+        return TARV;
+    } else if (programUID == MERE_UID) {
+        return MERE;
+    } else if (programUID == ENFANT_UID) {
+        return ENFANT;
+    }
+}
+
+
+function getEnrollmentUIDLabel(enroll) {
+    var program = getProgramName(enroll.program);
+    return program + "-" + enroll.enrollment;
+}
+
+function getEvents_format(events_data, stage) {
+    var events = {};
+    var stage_events = [];
+    stage_events = events_data.filter(function (entry) {
+        if (entry.programStage === stage ) {
+            return entry;
+        }
+    })
+    stage_events.forEach((event) => {
+        var obj = {};
+        var date;
+        if(event.status === "SCHEDULE") {
+            date = event.dueDate;
+        } else {
+            date = event.eventDate;
+        }
+        obj[getDHIS2dateFormat(date)] = event.event;
+        Object.assign(events, obj)
+    });
+    return events;
+}
+
+/*** Utils ***/
+function saveJSONFile(filename, json_data) {
+    try {
+        fs.writeFileSync(filename, JSON.stringify(json_data, null, 2));
+    } catch (err) {
+        // An error occurred
+        logger.error(err);
+    }
+}
+
+function getDHIS2dateFormat(date) {
+    return date.substr(0, 10);
+}
---
+++ retrieveDHISdata.js
@@ -0,0 +1,114 @@
+const https = require("https");
+const { logger, logger_fr } = require('./logger.js');
+const endpointConfig = require('./config.json');
+const { reject } = require("async");
+var fs = require('fs');
+
+//PROGRAMS
+const PROGRAM_TARV = "e3swbbSnbQ2";
+const PROGRAM_PTME_MERE = "MVooF5iCp8L";
+const PROGRAM_PTME_ENFANT = "PiXg9cX1i0y";
+
+//ORGUNITS
+/*const OU_MAPPING = {
+    "003BDI003S010912": "FG6tYYTDf5d", // Centre Akabanga Rumonge
+    "003BDI006S010120": "g9r6PbRYVAi", // Centre Akabanga Gitega
+    "003BDI010S020502": "nniW4f9J1tB", // Centre Akabanga Nyanza-Lac
+    "003BDI012S010120": "yW1SToCNaYm", // Centre Akabanga Muyinga
+    "003BDI013S010301": "IFiJar1g6y9", // Hôpital de Kibumbu
+    "003BDI014S010120": "ZAZapFNLXru", // Centre Akabanga Ngozi
+    "003BDI017S010401": "YIJAETW8k0a", // Centre Akabanga Bujumbura
+    "003BDI017S020203": "DLsHsaJhtnk", // Hôpital Militaire de Kamenge
+}*/
+const orgUnit = "DLsHsaJhtnk";
+
+//Main
+saveTEIs(orgUnit);
+
+//Get TEIs given a PROGRAM and an ORGUNIT
+async function getTEIs(programUID, orgUnit) {
+
+    logger.debug(programUID)
+    return new Promise((resolve) => {
+        let TEIs;
+        try {
+            let optionsgetmsg = {
+                host: endpointConfig.dhisServer, // here only the domain name (no http/https !)
+                port: 443,
+                path: '/api/trackedEntityInstances.json?program=' + encodeURIComponent(programUID) + '&ou=' + orgUnit + '&fields=*,enrollments&paging=false', // the rest of the url with parameters if needed
+                method: 'GET', // do GET
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Cache-Control': 'no-cache',
+                    'Pragma': 'no-cache'
+                },
+                auth: endpointConfig.dhisUser + ':' + endpointConfig.dhisPass
+            };
+
+            // do the GET request
+            let reqGet = https.request(optionsgetmsg, function (res) {
+                var bodyChunks = [];
+                res.on('data', function (chunk) {
+                    bodyChunks.push(chunk);
+                }).on('end', function () {
+                    let body = Buffer.concat(bodyChunks);
+                    let myJSONParse = JSON.parse(body);
+
+                    if (myJSONParse.length != 0) {
+                        TEIs = myJSONParse.trackedEntityInstances;
+                        resolve(TEIs)
+                    } else {
+                        TEIs = "";
+                        resolve(TEIs)
+                    }
+                })
+            });
+
+            reqGet.end();
+
+            reqGet.on('error', function (e) {
+                logger.error(e);
+                TEIs = "";
+                //resolve({ enrolledTEIs: TEIs })
+                reject(e)
+            });
+
+        } catch (error) {
+            TEIs = "";
+            //resolve({ enrolledTEIs: TEIs })
+            reject(e)
+        }
+    });
+}
+
+
+async function saveTEIs(orgUnit) {
+    //Get TEIs from 3 programs given an orgUnit
+    var enfant_teis = await getTEIs(PROGRAM_PTME_ENFANT, orgUnit).catch((err) => {
+        logger.error(err)
+    });
+
+    var mere_teis = await getTEIs(PROGRAM_PTME_MERE, orgUnit).catch((err) => {
+        logger.error(err)
+    });
+
+    var tarv_teis = await getTEIs(PROGRAM_TARV, orgUnit).catch((err) => {
+        logger.error(err)
+    });
+
+    /**** Save to JSON file ****/
+    const ENFANT_DHIS2_FILE = "DHIS2_data/enfant.json";
+    const MERE_DHIS2_FILE = "DHIS2_data/mere.json";
+    const TARV_DHIS2_FILE = "DHIS2_data/tarv.json";
+    try {
+        fs.writeFileSync(ENFANT_DHIS2_FILE, JSON.stringify(enfant_teis));
+        fs.writeFileSync(MERE_DHIS2_FILE, JSON.stringify(mere_teis));
+        fs.writeFileSync(TARV_DHIS2_FILE, JSON.stringify(tarv_teis));
+    } catch (err) {
+        // An error occurred
+        logger.error(err);
+    }
+}
+
+
+
