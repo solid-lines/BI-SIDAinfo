23b1d04ca9cd93ca567597b4c8ce09b68abbaa77
Carlos Tejo <info@dayures.net>
2022-03-02 16:32:17 +0100

    updated code for upload data to server. Use axios

--- uploadData.js
+++ uploadData.js
@@ -1,5 +1,5 @@
+const axios = require('axios');
 var fs = require('fs');
-const https = require("https");
 const { logger, logger_fr } = require('./logger.js');
 const endpointConfig = require('./config.json');
 
@@ -9,6 +9,18 @@ const SOURCE_OU_CODE = "003BDI017S020203";//TODO: parametrizar
 const SOURCE_DATE = "2021_08_05";//TODO: parametrizar
 const ACTIONS_FOLDER = "actions/" + SOURCE_OU_CODE;
 const ACTIONS_FILE = "actions/" + SOURCE_OU_CODE + "/actions.json";
+
+//Actions
+const CREATE = "CREATE";
+const DELETE = "DELETE";
+const UPDATE = "UPDATE";
+
+//Resource Types
+const TEI_TYPE = "TEI";
+const TEA_TYPE = "TEA";
+const ENROLLMENT_TYPE = "ENROLLMENT";
+const EVENT_TYPE = "EVENT";
+const DE_TYPE = "DE";
 
 //check if folder exists. If not, create it
 if (!fs.existsSync(ACTIONS_FOLDER)) {
@@ -20,102 +32,127 @@ var actions = JSON.parse(fs.readFileSync(ACTIONS_FILE)); //Array
 
 /********** Classify by resource type ******/
 
-var TEIs = getActionByType(actions, "TEI");
+var TEIs = getActionByResourceType(actions, TEI_TYPE);
 
-var enrollments = getActionByType(actions, "Enrollment");
+var TEAs = getActionByResourceType(actions, TEA_TYPE);
 
-var events = getActionByType(actions, "EVENT");
+var enrollments = getActionByResourceType(actions, ENROLLMENT_TYPE);
 
-var TEAs = getActionByType(actions, "TEA");
+var events = getActionByResourceType(actions, EVENT_TYPE);
 
-var DEs = getActionByType(actions, "DE");
+var DEs = getActionByResourceType(actions, DE_TYPE);
 
 
-/************** TEI actions upload *********************/
-
-/* Delete */
-var TEIs_toDelete = getActionByAction(TEIs, "DELETE");
-
-//En 2.33: borra TEI, enrollment pero no Events
-//TODO: borrar eventos
-TEIs_toDelete.forEach((TEI) => {
-    //deleteTEI(TEI); //TODO: descomentar
-});
+// /************** TEI actions upload *********************/
 
 /* Create */
-var TEIs_toCreate = getActionByAction(TEIs, "CREATE");
+
+const TEIs_toCreate = getActionByOperationType(TEIs, CREATE);
 
 TEIs_toCreate.forEach((TEI) => {
-
-    var payload = getTEIpayload(TEI);
-    logger.info(`Creating TEI ${TEI.resource}`);//, payload: ${JSON.stringify(payload)}`);
-    //createTEI(TEI, payload);//TODO: descomentar
+    const payload = getTEIpayload(TEI.uid);
+    logger.info(`Create TEI ${TEI.uid}, payload: ${JSON.stringify(payload)}`);
+    post_resource(TEI_TYPE, TEI.uid, payload);
 });
-
-/* Update */
-var TEIs_toUpdate = getActionByAction(TEIs, "UPDATE");
-TEIs_toUpdate.forEach((TEI) => {
-
-    var payload = getTEIpayload(TEI);
-    logger.info(`Updating TEI ${TEI.resource}`);//, payload: ${JSON.stringify(payload)}`);
-    //updateTEI(TEI);//TODO: descomentar
-});
-
-
-/************** TEA actions upload *********************/
 
 /* Delete */
-var TEAs_toDelete = getActionByAction(TEAs, "DELETE");
-TEAs_toDelete.forEach((TEA) => {
-    deleteTEA(TEA);
+// In 2.36, delete a TEI means: delete TEI, delete enrollment, delete events (but in 2.33, events were not deleted).
+const TEIs_toDelete = getActionByOperationType(TEIs, DELETE);
+TEIs_toDelete.forEach((TEI) => {
+    logger.info(`Delete TEI ${TEI.uid}`);
+    delete_resource(TEI_TYPE, TEI.uid)
 });
+
+// /************** TEA actions upload *********************/
+
+// TODO: improvement. send one PUT per TEI (no one PUT per each TEA)
+
 /* Create */
-var TEAs_toCreate = getActionByAction(TEAs, "CREATE");
+const TEAs_toCreate = getActionByOperationType(TEAs, CREATE);
 TEAs_toCreate.forEach((TEA) => {
-    createTEA(TEA);
+    const payload = getTEApayload(TEA.TEI);
+    logger.info(`Create TEA ${TEA.TEA} (TEI ${TEA.TEI}), payload: ${JSON.stringify(payload)}`);
+    put_resource(TEA_TYPE, TEA.TEI, payload);
 });
+
+
+/* Delete */
+const TEAs_toDelete = getActionByOperationType(TEAs, DELETE);
+TEAs_toDelete.forEach((TEA) => {
+    const payload = getTEApayload(TEA.TEI);
+    logger.info(`Delete TEA ${TEA.TEA} (TEI ${TEA.TEI}), payload: ${JSON.stringify(payload)}`);
+    put_resource(TEA_TYPE, TEA.TEI, payload);
+});
+
 /* Update */
-var TEAs_toUpdate = getActionByAction(TEAs, "UPDATE");
-//TODO: test TEAdeletion, TEAcreation and TEAupdate (no data in this ou dump)
+const TEAs_toUpdate = getActionByOperationType(TEAs, UPDATE);
+TEAs_toUpdate.forEach((TEA) => {
+    const payload = getTEApayload(TEA.TEI);
+    logger.info(`Update TEA ${TEA.TEA} (TEI ${TEA.TEI}), payload: ${JSON.stringify(payload)}`);
+    put_resource(TEA_TYPE, TEA.TEI, payload);
+});
+
 
 /************** Enrollment actions upload *********************/
 
-/* Delete */
-
 /* Create */
-
-/* Update */
-
-/************** Events actions upload *********************/
-
-/* Delete */
-
-/* Create */
-
-/* Update */
-
-/************** DataElements actions upload *********************/
+const enrollments_to_create = getActionByOperationType(enrollments, CREATE);
+enrollments_to_create.forEach((enrollment) => {
+    const enrollment_uid = enrollment.uid
+    const payload = get_enrollment_payload(enrollment.TEI, enrollment_uid);
+    logger.info(`Create enrollment ${enrollment_uid} (TEI ${enrollment.TEI}), payload: ${JSON.stringify(payload)}`);
+    post_resource(ENROLLMENT_TYPE, enrollment_uid, payload);
+});
 
 /* Delete */
-
-/* Create */
+const enrollments_to_delete = getActionByOperationType(enrollments, DELETE);
+enrollments_to_delete.forEach((enrollment) => {
+    const enrollment_uid = enrollment.uid
+    logger.info(`Delete enrollment ${enrollment_uid} (TEI ${enrollment.TEI})`);
+    delete_resource(ENROLLMENT_TYPE, enrollment_uid);
+});
 
 /* Update */
+//Enrollment_STATUS_UPDATE
+const enrollments_to_update = getActionByOperationType(enrollments, UPDATE);
+enrollments_to_update.forEach((enrollment) => {
+    const enrollment_uid = enrollment.uid
+    const payload = get_enrollment_status_payload(enrollment.TEI, enrollment_uid);
+
+    logger.info(`Update enrollment status ${enrollment_uid} (TEI ${enrollment.TEI}), payload: ${JSON.stringify(payload)}`);
+    put_resource(ENROLLMENT_TYPE, enrollment_uid, payload);
+});
+
+// /************** Events actions upload *********************/
+
+// /* Delete */
+
+// /* Create */
+
+// /* Update */
+
+// /************** DataElements actions upload *********************/
+
+// /* Delete */
+
+// /* Create */
+
+// /* Update */
 
 
 
-function getActionByType(actions, type) {
+function getActionByResourceType(actions, resource_type) {
     var actions = actions.filter((action) => {
-        if (action.type == type) {
+        if (action.type == resource_type) {
             return action;
         }
     });
     return actions;
 }
 
-function getActionByAction(actions, operation) {
+function getActionByOperationType(actions, operation_type) {
     var actions = actions.filter((action) => {
-        if (action.action == operation) {
+        if (action.action == operation_type) {
             return action;
         }
     });
@@ -123,127 +160,144 @@ function getActionByAction(actions, operation) {
 }
 
 
-/*************** API calls *****************/
-//When you delete a TEI, you delete all data associated with the TEI (except events?).
-async function deleteTEI(TEI) {
+// /*************** API calls *****************/
+async function post_resource(resource_type, uid, payload) {
+    
+    const mapping = {
+        [TEI_TYPE]: "trackedEntityInstances/",
+        [ENROLLMENT_TYPE]: "enrollments/"
+    }
 
-    logger.debug(`Deleting TEI ${TEI}`);
-    return new Promise((resolve) => {
 
-        try {
-            let optionsgetmsg = {
-                host: endpointConfig.dhisServer, // here only the domain name (no http/https !)
-                port: 443,
-                path: '/api/trackedEntityInstances/' + encodeURIComponent(TEI.uid),
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Cache-Control': 'no-cache',
-                    'Pragma': 'no-cache'
-                },
-                auth: endpointConfig.dhisUser + ':' + endpointConfig.dhisPass
-            };
+    logger.info(`POST ${resource_type} ${uid}. url '${mapping[resource_type]}'`);
 
-            // do the DELETE request
-            let reqDel = https.request(optionsgetmsg, function (res) {
-                var bodyChunks = [];
-                res.on('data', function (chunk) {
-                    bodyChunks.push(chunk);
-                }).on('end', function () {
-                    let body = Buffer.concat(bodyChunks);
-                    let myJSONParse = JSON.parse(body);
+    const config = {
+        baseURL: endpointConfig.dhis2Server,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache'
+        },
+        auth: {
+            username: endpointConfig.dhis2User,
+            password: endpointConfig.dhis2Pass
+        },
+        validateStatus: function (status) {
+            return status == 200; // TEI. not only create also update
+        },
+    }
 
-                    logger.info(myJSONParse);
-                    resolve(myJSONParse);
-                })
-            });
+    axios.post(mapping[resource_type], payload, config)
+      .then(function (response) {
+        logger.info(JSON.stringify(response.data))
+      })
+      .catch(function (error) {
+        logger.error(error.response.data)
+      });
 
-            reqDel.end();
-
-            reqDel.on('error', function (e) {
-                logger.error(e);
-                reject(e)
-            });
-
-        } catch (error) {
-            reject(e)
-        }
-    });
 }
 
-async function deleteTEA(TEA) {
+async function put_resource(resource_type, uid, payload) {
+    
+    const mapping = {
+        [TEA_TYPE]: "trackedEntityInstances/"+uid+"?program=MVooF5iCp8L", // TODO only needs a valid program uid
+        [ENROLLMENT_TYPE]: "enrollments/"+uid
+    }
 
-    logger.debug(`Deleting TEA ${TEA}`);
-    //TODO
+    logger.info(`PUT ${resource_type} ${uid}. url '${mapping[resource_type]}'`);
+
+    const config = {
+        baseURL: endpointConfig.dhis2Server,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache'
+        },
+        auth: {
+            username: endpointConfig.dhis2User,
+            password: endpointConfig.dhis2Pass
+        },
+        validateStatus: function (status) {
+            return status == 200;
+        },
+    }
+
+    axios.put(mapping[resource_type], payload, config)
+      .then(function (response) {
+        logger.info(JSON.stringify(response.data))
+      })
+      .catch(function (error) {
+        logger.error(error.response.data)
+      });
 }
 
-//TODO: body with payload
-//TODO: "Non-unique attribute value '003BDI017S020203002602' for attribute dsWUbqvV9GW"
-async function createTEI(TEI, payload) {
+async function delete_resource(resource_type, uid) {
+    
+    const mapping = {
+        [TEI_TYPE]: "trackedEntityInstances/"+uid,
+        [ENROLLMENT_TYPE]: "enrollments/"+uid
+    }
 
-    logger.debug(`Creating TEI ${TEI}`);
-    return new Promise((resolve) => {
+    logger.info(`DELETE ${resource_type} ${uid}. url '${mapping[resource_type]}'`);
 
-        try {
-            let optionsgetmsg = {
-                host: endpointConfig.dhisServer, // here only the domain name (no http/https !)
-                port: 443,
-                path: '/api/trackedEntityInstances/',
-                method: 'POST',
-                body: payload,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Cache-Control': 'no-cache',
-                    'Pragma': 'no-cache'
-                },
-                auth: endpointConfig.dhisUser + ':' + endpointConfig.dhisPass
-            };
+    const config = {
+        baseURL: endpointConfig.dhis2Server,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache'
+        },
+        auth: {
+            username: endpointConfig.dhis2User,
+            password: endpointConfig.dhis2Pass
+        },
+        validateStatus: function (status) {
+            return status == 200;
+        },
+    }
 
-            // do the POST request
-            let reqDel = https.request(optionsgetmsg, function (res) {
-                var bodyChunks = [];
-                res.on('data', function (chunk) {
-                    bodyChunks.push(chunk);
-                }).on('end', function () {
-                    let body = Buffer.concat(bodyChunks);
-                    let myJSONParse = JSON.parse(body);
-
-                    logger.info(myJSONParse);
-                    resolve(myJSONParse);
-                })
-            });
-
-            reqDel.end();
-
-            reqDel.on('error', function (e) {
-                logger.error(e);
-                reject(e)
-            });
-
-        } catch (error) {
-            reject(e)
-        }
-    });
+    axios.delete(mapping[resource_type], config)
+      .then(function (response) {
+        logger.info(JSON.stringify(response.data))
+      })
+      .catch(function (error) {
+        logger.error(error.response.data)
+      });
 }
-
-async function createTEA(TEA) {
-
-    logger.debug(`Creating TEA ${TEA}`);
-    //TODO
-}
-
 
 
 /********* Read TEI data ***********/
 
-function getTEIpayload(TEI) {
-    var uid = TEI.uid;
-    return readTEI(uid);
-
+function getTEIpayload(tei_uid) {
+    return readTEI(tei_uid);
 }
 
-function readTEI(TEI_uid) {
+function getTEApayload(tei_uid) {
+    let tei = readTEI(tei_uid);
+    delete tei['enrollments'] // remove enrollments key-value
+    return tei
+}
 
+
+function get_enrollment_payload(tei_uid, enrollment_uid){
+    const tei = readTEI(tei_uid);
+    const enrollment = tei['enrollments'].filter(function(enrol){
+        return enrol.enrollment == enrollment_uid;
+    })
+    return enrollment[0] // return one enrollment
+}
+
+function get_enrollment_status_payload(tei_uid, enrollment_uid){
+    let enrollment = get_enrollment_payload(tei_uid, enrollment_uid)
+    delete enrollment['events'] // remove events key-value
+    return enrollment
+}
+
+
+function readTEI(TEI_uid) {
     var TEI_file;
     const TEI_fileName = "./teis/" + SOURCE_OU_CODE + "_" + SOURCE_DATE + "/" + TEI_uid + ".json";
     if (!fs.existsSync(TEI_fileName)) {
@@ -253,5 +307,4 @@ function readTEI(TEI_uid) {
     }
 
     return TEI_file;
-
 }
